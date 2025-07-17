3daf0bbb1ee6f5a09dc7496dffea63826f3a808e
Tanmay Shinde <tanmayshinde1401@gmail.com>
2025-07-17 07:38:38 -0400

    implemented database connection logic

--- connection.py
+++ connection.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import sqlalchemy as db
 from dotenv import load_dotenv
 import os
 from pathlib import Path
@@ -11,6 +12,31 @@ from pathlib import Path
 # 5. Use os to extract DB_PASSWORD from the .env file
 
 
+def get_db():
+    parent_dir = Path('connection.py').resolve().parent
+    env_path = parent_dir / '.env'
+
+    load_dotenv(dotenv_path=env_path)
+
+    db_password = os.getenv("DB_PASSWORD")
+
+    try:
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",  # Replace with your MySQL username
+            password=db_password,  # Replace with your MySQL password
+            database="retaildb"  # Replace with your database name
+        )
+
+        if mydb.is_connected():
+            print("Successfully connected to MySQL database!")
+            return mydb
+
+    except mysql.connector.Error as err:
+        print(f"Error connecting to MySQL: {err}")
+        return None
+
+
 def get_engine():
     parent_dir = Path('connection.py').resolve().parent
     env_path = parent_dir / '.env'
@@ -18,9 +44,11 @@ def get_engine():
     load_dotenv(dotenv_path=env_path)
 
     db_password = os.getenv("DB_PASSWORD")
-    db_url = ("postgresql://neondb_owner:" +
-              str(db_password) +
-              "@ep-black-bird-a1vf17nb-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
+    db_url = ("mysql+mysqlconnector://root:" + str(db_password) + "@localhost/retaildb")
 
     engine = db.create_engine(url=db_url)
     return engine
+
+
+def close_db(mydb):
+    mydb.close()
